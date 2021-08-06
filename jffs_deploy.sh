5927b71df4eb75e9b66384dea97142f8b9491955
admin <admin@RT-AC68U-0338>
2021-08-06 04:54:15 +0000

    update cron to auto backup router

--- jffs_deploy.sh
+++ jffs_deploy.sh
@@ -5,7 +5,7 @@ src=/tmp/mnt/data/my_scripts/jffs
 
 /opt/bin/rsync -a ${src}/etc/profile /jffs/etc/profile
 /opt/bin/rsync -a $src/my_scripts/admin /jffs/my_scripts/
-# cp  $src/my_scripts/admin /jffs/my_scripts/admin
+cp /jffs/my_scripts/admin /var/spool/cron/crontabs/admin 
 /opt/bin/rsync -a $src/my_scripts/script_usbmount.sh /jffs/my_scripts/
 /opt/bin/rsync -a $src/my_scripts/script_usbumount.sh /jffs/my_scripts/
 /opt/bin/rsync -a $src/my_scripts/sync_time.sh /jffs/my_scripts/
